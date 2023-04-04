a4c38297bf916b9325dd7b7828981755c0d9ba3d
Zhrddn <98458089+Zhrddn@users.noreply.github.com>
2023-04-04 18:41:35 -0400

    project is set up & runned, get user by id is mocked & tested

--- src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
+++ src/main/java/com/spotlight/platform/userprofile/api/core/profile/persistence/UserProfileDaoInMemory.java
@@ -2,7 +2,11 @@ package com.spotlight.platform.userprofile.api.core.profile.persistence;
 
 import com.spotlight.platform.userprofile.api.model.profile.UserProfile;
 import com.spotlight.platform.userprofile.api.model.profile.primitives.UserId;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyName;
+import com.spotlight.platform.userprofile.api.model.profile.primitives.UserProfilePropertyValue;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,6 +16,15 @@ public class UserProfileDaoInMemory implements UserProfileDao {
 
     @Override
     public Optional<UserProfile> get(UserId userId) {
+
+        UserId userId1 = UserId.valueOf("1");
+        Map<UserProfilePropertyName, UserProfilePropertyValue> userProfilePropertyNameUserProfilePropertyMap = new ConcurrentHashMap<>();
+        userProfilePropertyNameUserProfilePropertyMap.put(UserProfilePropertyName.valueOf("id"), UserProfilePropertyValue.valueOf("1"));
+        userProfilePropertyNameUserProfilePropertyMap.put(UserProfilePropertyName.valueOf("name"), UserProfilePropertyValue.valueOf("Zaheer"));
+
+        UserProfile userProfile = new UserProfile(userId1, LocalDateTime.now().toInstant(ZoneOffset.UTC), userProfilePropertyNameUserProfilePropertyMap);
+        storage.put(userId1, userProfile);
+
         return Optional.ofNullable(storage.get(userId));
     }
 
