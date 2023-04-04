fece6e5e1411585720b7a0bd7a913855042a0218
Zaheer Ud Din <zaheeru7@gmail.com>
2023-04-04 17:25:29 -0400

    Initial commit

---
+++ src/main/java/com/spotlight/platform/userprofile/api/web/modules/ProfileModule.java
@@ -0,0 +1,15 @@
+package com.spotlight.platform.userprofile.api.web.modules;
+
+import com.google.inject.AbstractModule;
+
+import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDao;
+import com.spotlight.platform.userprofile.api.core.profile.persistence.UserProfileDaoInMemory;
+
+import javax.inject.Singleton;
+
+public class ProfileModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(UserProfileDao.class).to(UserProfileDaoInMemory.class).in(Singleton.class);
+    }
+}
