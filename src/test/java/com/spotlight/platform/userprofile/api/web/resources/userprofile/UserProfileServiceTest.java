9330827f0593b6805bb0cb43bc8bf444935fce3c
Zhrddn <98458089+Zhrddn@users.noreply.github.com>
2023-04-09 19:51:44 -0400

    unit test added for run command collect & increment case

--- src/test/java/com/spotlight/platform/userprofile/api/web/resources/userprofile/UserProfileServiceTest.java
+++ src/test/java/com/spotlight/platform/userprofile/api/web/resources/userprofile/UserProfileServiceTest.java
@@ -18,9 +18,7 @@ import ru.vyarus.dropwizard.guice.test.jupiter.TestDropwizardApp;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -33,8 +31,34 @@ class UserProfileServiceTest {
 //
 //    @InjectMocks
 //    UserProfileService userProfileService;
+
     @Test
     void runCommandOnUserProfile_replaceCommand(UserProfileService userProfileService, UserProfileDao userProfileDao) {
+        String USER_ID = "de4310e5-b139-441a-99db-77c9c4a5fada";
+        Map<UserProfilePropertyName, UserProfilePropertyValue> userProperties = new HashMap<>();
+        userProperties.put(UserProfilePropertyName.valueOf("currentGold"), UserProfilePropertyValue.valueOf(200));
+        UserProfile newUserProfile  = new UserProfile(UserId.valueOf(USER_ID), LocalDateTime.MAX.toInstant(ZoneOffset.UTC), userProperties);
+
+        userProfileDao.put(newUserProfile);
+
+
+        UserCommandDto userCommandDto = new UserCommandDto();
+        // Set valid properties for the command object
+        userCommandDto.setType("replace");
+        userCommandDto.setUserId(USER_ID);
+        HashMap<String, Object> properties = new HashMap<>();
+        properties.put("currentGold", 500);
+        userCommandDto.setProperties(properties);
+
+        UserProfile userProfile = userProfileService.get(UserId.valueOf(USER_ID));
+
+        UserProfile updatedUserProfile = userProfileService.runCommandOnUserProfile(userProfile, userCommandDto);
+
+        assertEquals(UserProfilePropertyValue.valueOf(500), updatedUserProfile.userProfileProperties().get(UserProfilePropertyName.valueOf("currentGold")));
+    }
+
+    @Test
+    void runCommandOnUserProfile_incrementCommand(UserProfileService userProfileService, UserProfileDao userProfileDao) {
         String USER_ID = "de4310e5-b139-441a-99db-77c9c4a5fada";
         Map<UserProfilePropertyName, UserProfilePropertyValue> userProperties = new HashMap<>();
         userProperties.put(UserProfilePropertyName.valueOf("battleFought"), UserProfilePropertyValue.valueOf(10));
@@ -59,20 +83,40 @@ class UserProfileServiceTest {
 
         assertEquals(UserProfilePropertyValue.valueOf(20), updatedUserProfile.userProfileProperties().get(UserProfilePropertyName.valueOf("battleFought")));
     }
-//
-//    @Test
-//    void runCommandOnUserProfile_incrementCommand() {
-//        UserProfile userProfile = /* Initialize a UserProfile instance with somePropertyName having an integer value */;
-//        UserCommandDto userCommandDto = new UserCommandDto();
-//        userCommandDto.setType("increment");
-//        Map<String, Object> properties = new HashMap<>();
-//        properties.put("somePropertyName", 5);
-//        userCommandDto.setProperties(properties);
-//
-//        UserProfile updatedUserProfile = userProfileService.runCommandOnUserProfile(userProfile, userCommandDto);
-//
-//        assertEquals(/* The incremented value */, updatedUserProfile.userProfileProperties().get(/* The key corresponding to somePropertyName */));
-//    }
+
+    @Test
+    void runCommandOnUserProfile_collectCommand(UserProfileService userProfileService, UserProfileDao userProfileDao) {
+        String USER_ID = "de4310e5-b139-441a-99db-77c9c4a5fada";
+        Map<UserProfilePropertyName, UserProfilePropertyValue> userProperties = new HashMap<>();
+        List<String> inventory = new ArrayList<>();
+        inventory.add("sword1");
+        inventory.add("sword2");
+        inventory.add("shield1");
+
+        userProperties.put(UserProfilePropertyName.valueOf("inventory"), UserProfilePropertyValue.valueOf(inventory));
+        UserProfile newUserProfile  = new UserProfile(UserId.valueOf(USER_ID), LocalDateTime.MAX.toInstant(ZoneOffset.UTC), userProperties);
+
+        userProfileDao.put(newUserProfile);
+
+
+        UserCommandDto userCommandDto = new UserCommandDto();
+        // Set valid properties for the command object
+        userCommandDto.setType("collect");
+        userCommandDto.setUserId(USER_ID);
+        HashMap<String, Object> properties = new HashMap<>();
+        List<String> updatedInventory = new ArrayList<>();
+        inventory.add("sword3");
+        properties.put("inventory", updatedInventory);
+        userCommandDto.setProperties(properties);
+
+        UserProfile userProfile = userProfileService.get(UserId.valueOf(USER_ID));
+
+        UserProfile updatedUserProfile = userProfileService.runCommandOnUserProfile(userProfile, userCommandDto);
+        UserProfilePropertyValue updateInventory = updatedUserProfile.userProfileProperties().get(UserProfilePropertyName.valueOf("inventory"));
+        List<String> invenotryList = (List<String>) updateInventory.getValue();
+
+        assertEquals(Boolean.TRUE, invenotryList.contains("sword3") );
+    }
 //
 //    @Test
 //    void runCommandOnUserProfile_collectCommand() {
